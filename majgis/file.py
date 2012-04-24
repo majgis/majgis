23f00edeff70467ce04c3167c808e17c1fbeb114
Michael A. Jackson <majgis@gmail.com>
2012-04-24 01:18:40 -0700

    multipleFileWriterWithBuffer function added to majgis.file for writing to a large number of files.
    Ultimately, a version without a buffer was used, this funciton must be tracked down and added.

--- majgis/file.py
+++ majgis/file.py
@@ -104,3 +104,137 @@ def readMultipleFileLinesAndPositions(filePath,startPosition=None, bytesToRead=1
     
     return lines, position 
 
+
+class multipleFileWriterWithBuffer(): 
+    """ Write lines to multiple files efficiently. 
+     
+        Maintains a buffer for each file.  
+        Writes to disk when length of all strings in buffer exceeds strLen parameter. 
+        Creating new strings is inefficient, so buffer is stored as list of strings. 
+         
+    """ 
+    
+    def __init__(self, filePaths=[], outDir=None, initialDelete=True, strLen=8092): 
+        """ Initializer """ 
+ 
+        # Class level variables 
+        self.strLen = strLen 
+        self.filePathDict = {} 
+        self.outDir = outDir 
+        
+        if filePaths: 
+            self.addFiles(filePaths) 
+        
+        # Delete files by default, unless default overridden  
+        if initialDelete: 
+            self.deleteAllFiles(False) 
+ 
+            
+    def __del__(self): 
+        """ Destructor  
+         
+            Buffer is flushed when object is destroyed 
+        """ 
+        
+        self.flush() 
+ 
+ 
+    def __writeToFile(self, filePath, lst): 
+        """ Write lines to file 
+             
+            Write lines joined by the delimiter. 
+        """ 
+        
+        if not self.outDir is None: 
+            filePath = os.path.join(self.outDir, filePath) 
+        
+        open(filePath,'a').writelines(lst) 
+        
+        
+    def writeMultipleFileLines(self, filePaths, liness): 
+        """ Write lines to multiple files 
+         
+            liness: list of lists, one for each filePath 
+            Buffer is written to file when buffer exceeds strLen 
+            Line separators are not added. 
+        """ 
+        
+        for i,filePath in enumerate(filePaths): 
+            self.writeSingleFileLines(filePath,liness[i]) 
+            
+            
+    def writeSingleFileLines(self, filePath, lines):   
+        """ Add lines in iterable to buffer 
+         
+            Buffer is written to file when buffer exceeds strLen 
+            Line separators are not added 
+        """ 
+        
+        lenAndBuffer = self.filePathDict[filePath]     
+ 
+        for line in lines: 
+            lenAndBuffer[1].append(line) 
+            lenAndBuffer[0] = lenAndBuffer[0] + len(line) 
+                
+        if lenAndBuffer[0] > self.strLen: 
+            self.__writeToFile(filePath,lenAndBuffer[1]) 
+            lenAndBuffer[0] = 0 # set bytes to zero  
+            lenAndBuffer[1] = [] 
+    
+        
+    def writeMultipleFiles(self, filePaths, ss): 
+        """ Write strings to multiple files """ 
+        
+        for i,filePath in enumerate(filePaths): 
+            self.writeSingleFileLines(filePath, [ss[i]]) 
+ 
+ 
+    def writeSingleFile(self, filePath, s): 
+        """" Write string to file. """ 
+        
+        self.writeSingleFileLines(filePath, [s]) 
+        
+ 
+    def flush(self, write=True): 
+        """ Flush all buffers, writing to files if write parameter is True""" 
+ 
+        for filePath, (_strLen,fileBuffer) in self.filePathDict.items(): 
+            if write: 
+                self.__writeToFile(filePath, fileBuffer) 
+            del fileBuffer[:] 
+            
+            
+    def deleteAllFiles(self, flush=True): 
+        """ Delete all files 
+         
+            Flush buffers if flush parameter is true, the default. 
+        """ 
+        
+        if flush: 
+            self.flush(False) 
+ 
+        for filePath in self.filePathDict.keys(): 
+            if self.outDir is None: 
+                fullPath = filePath 
+            else: 
+                fullPath = os.path.join(self.outDir,filePath) 
+                
+            if os.path.exists(fullPath): 
+                os.remove(fullPath) 
+    
+    
+    def addFile(self, filePath): 
+        """ Add file as file Path 
+         
+            Files are stored in a dictionary with filePath as key 
+            and a list containing [string length integer, list of lines] as the value. 
+        """    
+        
+        self.filePathDict[filePath] = [0,[]] 
+            
+        
+    def addFiles(self, filePaths): 
+        """ Add files as iterable of full file paths """ 
+        
+        for filePath in filePaths: 
+            self.addFile(filePath) 
